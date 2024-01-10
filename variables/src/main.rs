0642a85eef4bf2aae028b487cf9e1343e122766d
Matt Stucky <59749326+mnstucky@users.noreply.github.com>
2024-01-10 18:21:14 +0000

    Variables

---
+++ variables/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    // let mut x: u8 = 255;
+    // println!("The value of x is {x}");
+    // x = match x.checked_add(1) {
+    //     None => 255,
+    //     Some(y) => y,
+    // };
+    // println!("The value of x is {x}");
+    // x = 255;
+    // x = x.saturating_add(1);
+    // println!("The value of x is {x}");
+    // x = 255;
+    // x = x.wrapping_add(1);
+    // println!("The value of x is {x}");
+
+    // let tup = ("Hello", 1, true);
+    // let (one, two, three) = tup;
+
+    // println!("{one}, {two}, {three}");
+
+    let array = [1, 2, 3];
+    println!("{:?}", array)
+}
